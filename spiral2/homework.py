ddb89c246f51ad56cff1280de3c921a256a71ddf
dupertba <74461690+dupertba@users.noreply.github.com>
2021-05-02 18:07:15 -0400

    Update homework.py

--- spiral2/homework.py
+++ spiral2/homework.py
@@ -1,4 +1,21 @@
 def spiralize(size, n=1):
-    """ Your code goes somewhere in here"""
-    return_value = n
-    return return_value
+    x = n # starting number
+    y = 2 #turn
+    z = n #current number in progress
+    r = 0 #used as a reset
+
+    while z < pow(size, 2) + n - 1:
+        z += y
+        x += z
+        r += 1
+        if r == 4:
+            y += 2
+            r = 0
+    return x
+
+def main():
+    test = spiralize(131,19)
+    print (test)
+    
+if __name__== "__main__":
+    main()
